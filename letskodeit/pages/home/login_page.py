0692eb5675fd576d88e79216ff03fae689ac9224
rawatmanish1989 <64397155+rawatmanish1989@users.noreply.github.com>
2020-05-17 18:47:36 +0530

    Creation of generic selenium_driver

--- letskodeit/pages/home/login_page.py
+++ letskodeit/pages/home/login_page.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
+from base.selenium_driver import SeleniumDriver
 
-class LoginPage():
+class LoginPage(SeleniumDriver):
     
     def __init__(self, driver):
+        super().__init__(driver)
         self.driver = driver
     
     # Locators
